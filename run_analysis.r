77f8d21c5f06f26085115251e98b8a66deb20232
chjacamp <chjacamp@gmail.com>
2015-10-25 01:29:30 -0600

    Functional but need to replace activity names.

--- run_analysis.r
+++ run_analysis.r
@@ -14,11 +14,56 @@
 
 ##  Read data into R and pipe to DPLYR's tbl_df() for further operations.
 
-test <- fread("UCI\ HAR\ Dataset\test\x_test.txt")
-test %>% tbl_df()
+## REMINDER FOR GRADERS: PIPING (SEE SWIRL TUTORIAL) ALLOWS YOU TO PASS 
+## AN OBJECT TO THE FUNCTION ON THE LEFT. PARENTHESES ARE NOT REQUIRED
+## IF THERE EXISTS SOLELY (1) ARGUMENT.
 
-train <- fread("UCI\ HAR\ Dataset\train\x_train.txt")
-train %>% tbl_df()
+test <- "./test/x_test.txt" %>% fread %>% tbl_df
+test_activity <- "./test/y_test.txt" %>% fread
+test_subject_id <- "./test/subject_test.txt" %>% fread
+
+train <- "./train/x_train.txt" %>% fread %>% tbl_df
+train_activity <- "./train/y_train.txt" %>% fread
+train_subject_id <- "./train/subject_train.txt" %>% fread
+
+##this one needs some work to function as column names - we
+##need to transpose them into a row.
+measurements <- 
+  "features.txt" %>% 
+  fread %>% 
+  tbl_df %>% 
+  t
+
+##measurements still needs a little work
+measurements <- measurements[2,]
+measurements <- c("subjectID", "activity",measurements)
+
+##bind us those col's
+test <- bind_cols(test_subject_id, test_activity, test)
+train <- bind_cols(train_subject_id, train_activity, train)
+
+##add us those column names
+colnames(test) <- measurements
+colnames(train) <- measurements
+
+all <- rbind(test, train)
+
+##Create a list of duplicate column names from measurements
+summer <- grepl("bandsEnergy", measurements)
+
+##Remove duplicate columns from data frame
+all <- all[!summer]
+
+##Piping so easy that Mario might get bored - 
+all %>% 
+  tbl_df %>%
+  arrange(subjectID, activity) %>%
+  select(subjectID, activity, contains("mean"), contains("std")) %>%
+  group_by(subjectID, activity) %>%
+  summarise_each(funs(mean)) %>%
+  print
+
+
 
 
 
