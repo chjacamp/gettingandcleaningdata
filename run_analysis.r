5e4c8541f57186d3ba72fba72f9b73da9fa67298
chjacamp <chjacamp@gmail.com>
2015-10-25 10:48:40 -0600

    Better descriptions and write.table exports txt file for grading.

--- run_analysis.r
+++ run_analysis.r
@@ -1,23 +1,9 @@
-## You should create one R script called run_analysis.R that does the 
-## following. 
-## 1)Merges the training and the test sets to create one data set.
-## 2)Extracts only the measurements on the mean and standard deviation 
-##  for each measurement. 
-## 3) Uses descriptive activity names to name the activities in the data set
-## 4) Appropriately labels the data set with descriptive variable names. 
-## 5) From the data set in step 4, creates a second, independent 
-##    tidy data set with the average of each variable for each activity
-##    and each subject.
-
 ##  Assume data has been loaded into working directory per assignment
 ##  instructions. 
 
 ##  Read data into R and pipe to DPLYR's tbl_df() for further operations.
-
-## REMINDER FOR GRADERS: PIPING (SEE SWIRL TUTORIAL) ALLOWS YOU TO PASS 
-## AN OBJECT TO THE FUNCTION ON THE LEFT. PARENTHESES ARE NOT REQUIRED
-## IF THERE EXISTS SOLELY (1) ARGUMENT.
-
+##  If you haven't seen the pipe operator ( %>% ) before, I recommended the
+##  swirl tutorials.
 test <- "./test/x_test.txt" %>% fread %>% tbl_df
 test_activity <- "./test/y_test.txt" %>% fread
 test_subject_id <- "./test/subject_test.txt" %>% fread
@@ -26,16 +12,14 @@ train <- "./train/x_train.txt" %>% fread %>% tbl_df
 train_activity <- "./train/y_train.txt" %>% fread
 train_subject_id <- "./train/subject_train.txt" %>% fread
 
-## This one needs some work to function as descriptive variable names - we
-## need to transpose them into a row.
-
+## Feature.txt functions as descriptive variable names - we
+## need to transpose them into a row and add subjectID and activity.
 measurements <- 
   "features.txt" %>% 
   fread %>% 
   tbl_df %>% 
   t
 
-## Measurements still need a little work:
 measurements <- measurements[2,]
 measurements <- c("subjectID", "activity",measurements)
 
@@ -50,17 +34,17 @@ colnames(train) <- measurements
 ## Merges the training and test sets
 all <- rbind(test, train)
 
-## Creates a list of duplicate column names from measurements
+## Creates a boolean vector to remove problematic "bandsEnergy" measurements.
 summer <- grepl("bandsEnergy", measurements)
-
-## Removes duplicate columns from data frame
 all <- all[!summer]
 
-##Piping so easy that Mario might get bored - 
+## Select any measurement columns containing mean or standard deviations.
+## Group by subjectID and activity, summarise the other columns by their
+## means.
 all %>% 
   tbl_df %>%
   arrange(subjectID, activity) %>%
   select(subjectID, activity, contains("mean"), contains("std")) %>%
   group_by(subjectID, activity) %>%
   summarise_each(funs(mean)) %>%
-  print
+  write.table(file="../gettingandcleaningdata.txt", row.names=F)
